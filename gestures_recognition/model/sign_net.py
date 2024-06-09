eb21fd715c436d392303f9206f145473e0312e7d
acodered <alexkondratru@gmail.com>
2024-06-09 17:15:17 +0200

    Update sign net architecture

--- gestures_recognition/model/sign_net.py
+++ gestures_recognition/model/sign_net.py
@@ -4,8 +4,10 @@ from typing import Tuple
 from keras.utils import image_dataset_from_directory
 from keras.callbacks import ModelCheckpoint, TensorBoard
 import time
+import keras
 
-SEED = 111
+SEED = 123
+N_CLASSES = 5
 
 class SignNet:
     def __init__(
@@ -16,17 +18,27 @@ class SignNet:
         self.model = Sequential()
         self.model.add(Conv2D(32, (3, 3), activation="relu", input_shape=input_shape))
         self.model.add(MaxPooling2D((2, 2)))
+        self.model.add(Conv2D(32, (3, 3), activation="relu"))
+        self.model.add(MaxPooling2D((2, 2)))
+        self.model.add(Conv2D(64, (3, 3), activation="relu"))
+        self.model.add(MaxPooling2D((2, 2)))
         self.model.add(Conv2D(64, (3, 3), activation="relu"))
         self.model.add(MaxPooling2D((2, 2)))
         self.model.add(Conv2D(128, (3, 3), activation="relu"))
         self.model.add(MaxPooling2D((2, 2)))
+        self.model.add(Conv2D(128, (3, 3), activation="relu"))
+        # self.model.add(MaxPooling2D((2, 2)))
+        self.model.add(keras.layers.Dropout(0.5))
         self.model.add(Flatten())
-        self.model.add(Dense(128, activation="relu"))
+        self.model.add(Dense(64, activation="relu"))
+        self.model.add(Dense(64, activation="relu"))
         self.model.add(Dense(n_classes, activation="softmax"))
 
         self.model.compile(
             optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
         )
+        
+        self.model.summary()
 
     def train(
         self,
@@ -45,6 +57,7 @@ class SignNet:
             batch_size=batch_size,
             validation_split=validation_split,
             seed=SEED,
+            shuffle=True
         )
 
         name = f"sign_net-{int(time.time())}{additional_data}"
@@ -66,5 +79,5 @@ class SignNet:
 
 
 if __name__ == "__main__":
-    sign_net = SignNet(n_classes=3)
-    sign_net.train("data", epochs=10, validation_split=0.2)
+    sign_net = SignNet(n_classes=N_CLASSES)
+    sign_net.train("data", epochs=10, validation_split=0.1, batch_size=64)
